c8ed898015de6c3d2be9132e8c9ba1d28039d0f4
WorpholeStudios <123596348+FallenSunArcade@users.noreply.github.com>
2023-10-16 05:26:40 -0600

    Init Repo

---
+++ Source/HTL/HTL.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class HTL : ModuleRules
+{
+	public HTL(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}

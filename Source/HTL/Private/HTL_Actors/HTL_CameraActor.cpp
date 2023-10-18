d4e411405884fe46e4c53db9ebeb9ac6da53ed60
WorpholeStudios <123596348+FallenSunArcade@users.noreply.github.com>
2023-10-18 07:29:58 -0600

    Adding Camera for Menu

---
+++ Source/HTL/Private/HTL_Actors/HTL_CameraActor.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HTL_Actors/HTL_CameraActor.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+AHTL_CameraActor::AHTL_CameraActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Scene Root"));
+	SetRootComponent(SceneComponent);
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+void AHTL_CameraActor::BeginPlay()
+{
+	Super::BeginPlay();
+	PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+	PlayerController->SetViewTargetWithBlend(this);
+}
+
+void AHTL_CameraActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/HTL/Public/HTL_Actors/HTL_CameraActor.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HTL_CameraActor.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class HTL_API AHTL_CameraActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AHTL_CameraActor();
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	USpringArmComponent* GetSpringArmComponent() const { return SpringArmComponent; }
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USceneComponent* SceneComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	float MaxSpeed;
+
+	UPROPERTY()
+	APlayerController* PlayerController;
+};
